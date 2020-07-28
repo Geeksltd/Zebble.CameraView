2be913e35f120620eb1685812c6a306358bf215c
Siavash Mohseni <Siavash.Mohseni@live.com>
2020-07-28 17:51:23 +0100

    camera stream

--- Shared/CameraView.cs
+++ Shared/CameraView.cs
@@ -1,7 +1,21 @@
-﻿namespace Zebble
+﻿using System;
+using System.Threading.Tasks;
+namespace Zebble
 {
     public class CameraView : View, IRenderedBy<CameraViewRenderer>
     {
+        public bool DisablePreview { get; set; } 
+
         public CameraView() => this.Size(100.Percent());
+                
+        internal event Action<TaskCompletionSource<byte[] >> RequestFrameCapture;
+
+        public Task< byte[] > CaptureFrame()
+        {
+            var source = new TaskCompletionSource<byte[]>();
+            RequestFrameCapture?.Invoke(source);
+            return source.Task;
+        }
     }
+
 }
--- iOS/IosCameraView.cs
+++ iOS/IosCameraView.cs
@@ -1,19 +1,97 @@
 namespace Zebble
 {
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using AVFoundation;
+    using CoreFoundation;
+    using CoreGraphics;
+    using CoreMedia;
+    using CoreVideo;
     using Foundation;
     using UIKit;
+
+    public class LocalAVCaptureStillImageOutput : AVFoundation.AVCaptureVideoDataOutputSampleBufferDelegate
+    {
+        CameraView Camera;
+        byte[] CurrentImageBuffer;
+        DateTime LastFrameCaptured;
+
+        public LocalAVCaptureStillImageOutput(CameraView camera)
+        {
+            Camera = camera;
+            camera.RequestFrameCapture += Camera_RequestFrameCapture;
+            LastFrameCaptured = DateTime.Now;
+        }
+
+        bool ignoreFirstFrame = true;
+        public override void DidOutputSampleBuffer(AVCaptureOutput captureOutput, CMSampleBuffer sampleBuffer, AVCaptureConnection connection)
+        {
+            if (connection.SupportsVideoOrientation)
+            {      
+                // TODO: check if we need this?
+                connection.VideoOrientation = 
+                    Device.Screen.Orientation == DeviceOrientation.Portrait ? 
+                    AVCaptureVideoOrientation.Portrait : AVCaptureVideoOrientation.LandscapeLeft;
+            }
+
+            if (DateTime.Now < LastFrameCaptured.AddSeconds(0.1)) return; // capture 10 frames per second
+            LastFrameCaptured = DateTime.Now;
+
+            // TODO: check if we need this?
+            if (ignoreFirstFrame)
+            {
+                ignoreFirstFrame = false;
+                return; 
+            }
+
+            CurrentImageBuffer = GetBytes(sampleBuffer.GetImageBuffer() as CVPixelBuffer);
+            sampleBuffer.Dispose();            
+        }
+
+        async void Camera_RequestFrameCapture(TaskCompletionSource<byte[]> source)
+        {
+            var buffer = CurrentImageBuffer ?? new byte[0];            
+            source.TrySetResult(buffer);            
+        }
+        byte[] GetBytes(CVPixelBuffer pixelBuffer)
+        {
+            pixelBuffer.Lock(CVPixelBufferLock.None);
+
+            var baseAddress = pixelBuffer.BaseAddress;
+            var bytesPerRow = pixelBuffer.BytesPerRow;
+            var width = pixelBuffer.Width;
+            var height = pixelBuffer.Height;
+            var log = baseAddress + " " + bytesPerRow + " " + width + " " + height;
+            Zebble.Device.Log.Message(log);
+            
+            var flags = CGBitmapFlags.PremultipliedFirst | CGBitmapFlags.ByteOrder32Little;
+            // Create a CGImage on the RGB colorspace from the configured parameter above
+            using (var cs = CGColorSpace.CreateDeviceRGB())
+            using (var context = new CGBitmapContext(baseAddress, width, height, 8, bytesPerRow, cs, flags))
+            using (var cgImage = context.ToImage())
+            {
+                pixelBuffer.Unlock(CVPixelBufferLock.None);
+                var capturedImage = UIImage.FromImage(cgImage);
+                 return capturedImage.AsPNG().ToArray();
+            }
+            
+        }
+    }
 
     class IosCameraView : UIView
     {
         CameraView Camera;
         AVCaptureSession CaptureSession;
+        LocalAVCaptureStillImageOutput Receiver;
 
         public IosCameraView(View view)
         {
             Camera = (CameraView)view;
+
             Camera.WhenShown(() => Thread.UI.Run(SetupLiveCameraStream));
+            Receiver = new LocalAVCaptureStillImageOutput(Camera);
         }
 
         public async Task SetupLiveCameraStream()
@@ -24,17 +102,34 @@ namespace Zebble
 
             var captureDevice = CreateCaptureDevice();
             if (captureDevice == null) return;
-
+            
             CaptureSession = new AVCaptureSession();
+            CaptureSession.BeginConfiguration();
+            CaptureSession.SessionPreset = AVCaptureSession.PresetMedium;
             CaptureSession.AddInput(AVCaptureDeviceInput.FromDevice(captureDevice));
-            CaptureSession.AddOutput(new AVCaptureStillImageOutput { OutputSettings = new NSDictionary() });
+            CaptureSession.AddOutput(CreateVideoDataOutput());
+            CaptureSession.CommitConfiguration();
             CaptureSession.StartRunning();
 
-            Layer.AddSublayer(new AVCaptureVideoPreviewLayer(CaptureSession)
-            {
-                Frame = Frame,
-                VideoGravity = AVLayerVideoGravity.Resize
-            });
+            if (!Camera.DisablePreview)
+                Layer.AddSublayer(new AVCaptureVideoPreviewLayer(CaptureSession)
+                {
+                    Frame = Frame,
+                    VideoGravity = AVLayerVideoGravity.Resize
+                });
+        }
+
+        private AVCaptureOutput CreateVideoDataOutput()
+        {
+            var result = new AVCaptureVideoDataOutput 
+            { 
+                AlwaysDiscardsLateVideoFrames = true ,
+                WeakVideoSettings = new CVPixelBufferAttributes { PixelFormatType = CVPixelFormatType.CV32BGRA }.Dictionary
+            };
+            
+            result.SetSampleBufferDelegateQueue(Receiver, DispatchQueue.MainQueue);
+            
+            return result;
         }
 
         async Task<bool> AuthorizeCameraUse()
